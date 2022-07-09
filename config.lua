ef7f86baa9a276d5b089e57ce51e67f7123d162e
Shikamaru <92938408+ArsamDev@users.noreply.github.com>
2022-07-09 12:27:17 +0200

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,8 @@
+Config = {}
+-- Das Geld was man bekommt wenn man Gewinnt!
+Config.winMoney = 5000
+-- Das Geld was man bekommt wenn man verliert!
+Config.canLose = false -- Wenn der Spieler verliert, bekommt er dann Geld?
+Config.loseMoney = 1000
+
+
